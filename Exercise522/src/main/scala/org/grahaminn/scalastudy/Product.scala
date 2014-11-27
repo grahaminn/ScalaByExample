4f7f6f89aaff5b45e5f185a0deb780d950b3df3e
Graham Innocent <grahaminnocent@gmail.com>
2014-11-27 12:27:56 +0000

    update func name

--- Exercise522/src/main/scala/org/grahaminn/scalastudy/Product.scala
+++ Exercise522/src/main/scala/org/grahaminn/scalastudy/Product.scala
@@ -1,7 +1,7 @@
 package org.grahaminn.scalastudy
 
 object Product {
-  def sum(f: Int => Int) (a: Int, b: Int): Int = {
+  def product(f: Int => Int) (a: Int, b: Int): Int = {
     def iter(a: Int, result: Int): Int = {
       if (a > b) result
       else iter (a+1, result * f(a))
