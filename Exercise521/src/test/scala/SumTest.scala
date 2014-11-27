514a2b74b5e236d79b5b9d233bdd41eea53e34c8
Graham Innocent <grahaminnocent@gmail.com>
2014-11-27 12:49:56 +0000

    use fold to correct test

--- Exercise521/src/test/scala/SumTest.scala
+++ Exercise521/src/test/scala/SumTest.scala
@@ -3,5 +3,5 @@ import org.scalacheck.Prop.forAll
 import org.grahaminn.scalastudy.Sum
 
 object SumTest extends Properties("Sum") {
-  property("sum") = forAll { (a: Int, b: Int) => Sum.sum(x => x)(a, b) == a+b }
+  property("sum") = forAll { (a: Int, b: Int) => Sum.sum(x => x)(a, b) == (a to b toList).foldLeft(0)((a,b) => a+b) }
 }
