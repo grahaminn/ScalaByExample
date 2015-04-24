0b871ee824dd7223ce99d5c47339745dacc1aebe
Innocent <grahamin@60f81db59b6c.ant.amazon.com>
2015-04-24 12:22:28 +0100

    exercise 7.2.2

---
+++ Exercise722-graham/src/main/scala/org/grahaminn/scalastudy/IntTree.scala
@@ -0,0 +1,17 @@
+abstract class IntTree {
+	def contains(t: IntTree, v: Int): Boolean = t match {
+		case EmptyTree() => false
+		case Node(node, left, right) = 
+			(node == v) || contains(left, v) || contains (right, v)
+	}
+
+	def insert(t: IntTree, v: Int): IntTree = t match {
+		case EmptyTree() => Node(v, EmptyTree(), EmptyTree())
+		case Node(node, left, right) => 
+			if (v < node) new Node(node, insert(left, v), right)
+			else new Node (node, left, insert(right, v)
+	}
+}
+
+case object EmptyTree extends IntTree
+case class Node(elem: Int, left: IntTree right: IntTree) extends IntTree
